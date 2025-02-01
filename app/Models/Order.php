c07be51224c06533520e26472f36453b4d9b85d2
erdemaltu <e-altug@hotmail.com>
2025-02-01 20:18:13 +0300

    Migration and model files were created

---
+++ app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['customer_id', 'total'];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
+
